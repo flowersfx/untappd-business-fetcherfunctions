588f6e68e5925c19120108de8889f1e982dfffd1
Oskar Duveborn <oskar.duveborn@valtech.com>
2025-10-01 17:20:27 +0200

    removed legacy from food function

--- GetFoodMenu.cs
+++ GetFoodMenu.cs
@@ -46,7 +46,7 @@ namespace FlowersFX
         public static async Task<MenuRoot> GetUntappdCustomMenu(IConfigurationRoot config)
         {
             var untappdFoodMenuId = config["UNTAPPD_FOOD_MENU_ID"];
-            var url = $"https://business.untappd.com/api/v1/menus/{untappdFoodMenuId}?full=true&include_legacy=true";
+            var url = $"https://business.untappd.com/api/v1/menus/{untappdFoodMenuId}?full=true";
             var json = await utilities.Get(url);
             dynamic parsed = JsonConvert.DeserializeObject(json);
 
